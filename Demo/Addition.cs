9cd9497dae0b10c492cbfff5abfe21742abd8432
unknown <pavanyalala>
2021-07-19 10:09:38 +0530

    [pavan] : Add program is initialized

---
+++ Demo/Addition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+   public class Addition
+    {
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Sum :"+c);
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Addition addition = new Addition();
+            addition.Add(5,10);
+        }
+    }
+}
